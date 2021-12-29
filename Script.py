890141f570d3742be998d69277c4ef20e95d16af
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2021-12-29 13:45:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,8 +11,8 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- Sorry Buddy This is not an Open Source Bot.
+- Message @Rex_Bots_Support (Only Reports)
 
 <b>DEVS:</b>
 - <a href=https://t.me/REX_BOTZ>𝚃𝚎𝚊𝚖 𝚁𝚎𝚡 𝚋𝚘𝚝𝚜</a>"""
